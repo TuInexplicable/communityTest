d7f7cc58e822c6843a4c23d1f9e289d6a61ebdd7
codeDriver <2252348117@qq.com>
2020-02-03 21:20:30 +0800

    common and login

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,1 +1,2 @@
 var baseUrl = 'http://localhost:8889';
+var gitUrl = "https://github.com/login/oauth/authorize?client_id=15fb5763897f211c9aa5&redirect_uri=http://locahost:8889/callback&scope=user&state=1";
